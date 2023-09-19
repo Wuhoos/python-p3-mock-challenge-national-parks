d390eca69b82a038d36bca27bd6e5bc2880ade3d
David <wjw3298@gmail.com>
2023-09-19 12:10:30 -0400

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,21 +2,34 @@ class NationalPark:
 
     def __init__(self, name):
         self.name = name
+        self.trip_list = []
+        self.visitor_list = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name_parameter):
+        if (not hasattr(self, 'name')) and isinstance(name_parameter, str) and len(name_parameter) >= 3:
+            self._name = name_parameter
         
     def trips(self):
-        pass
+        return self.trip_list
     
     def visitors(self):
-        pass
+        return self.visitor_list
     
     def total_visits(self):
-        pass
+        return len(self.trip_list)
     
     def best_visitor(self):
-        pass
+        return max(self.visitor_list, key=lambda v: len([trip for trip in v.trip_list if trip.national_park is self]))
 
 
 class Trip:
+
+    all = []
     
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
@@ -24,17 +37,80 @@ class Trip:
         self.start_date = start_date
         self.end_date = end_date
 
+        self.visitor.trip_list.append(self)
+
+        if not (national_park in self.visitor.park_list):
+            self.visitor.park_list.append(national_park)
+
+        self.national_park.trip_list.append(self)
+
+        if not (visitor in self.national_park.visitor_list):
+            self.national_park.visitor_list.append(visitor)
+
+        Trip.all.append(self)
+
+
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, new_date):
+        if type(new_date) == str and len(new_date) >= 7:
+            self._start_date = new_date
+
+    @property
+    def end_date(self):
+        return self._end_date
+    
+    @end_date.setter
+    def end_date(self, date):
+        if isinstance(date, str) and len(date) >= 7:
+            self._end_date = date
+
+    @property
+    def visitor(self):
+        return self._visitor
+    
+    @visitor.setter
+    def visitor(self, visitor):
+        if type(visitor) == Visitor:
+            self._visitor = visitor
+
+    @property
+    def national_park(self):
+        return self._national_park
+    
+    @national_park.setter
+    def national_park(self, park):
+        if type(park) == NationalPark:
+            self._national_park = park
+
+
 
 class Visitor:
 
     def __init__(self, name):
         self.name = name
+        self.trip_list = []
+        self.park_list = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name_parameter):
+        if type(name_parameter) == str and 1 <= len(name_parameter) <= 15:
+            self._name = name_parameter
         
     def trips(self):
-        pass
+        return self.trip_list
     
     def national_parks(self):
-        pass
+        return self.park_list
     
     def total_visits_at_park(self, park):
-        pass
+        if not park.visitors():
+            return 0
+        return len([trip for trip in self.trip_list if trip.national_park == park])
